675ae63cbc28e83a9e26bc46a1b30147e97385ae
Kalen Jordan <kalen@kalens-laptop.lan>
2024-04-26 04:47:04 -0500

    Title change

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 import enTranslations from '@shopify/polaris/locales/en.json';
-import {AppProvider, Page, LegacyCard, Button} from '@shopify/polaris';
+import {AppProvider, Page, Card, Button} from '@shopify/polaris';
 import '@shopify/polaris/build/esm/styles.css';
 
 function App() {
   return (
     <AppProvider i18n={enTranslations}>
-    <Page title="Shopify To Mechanic Email Template Migrator">
-      <LegacyCard sectioned>
+    <Page title="Shopify To Mechanic Email Template Converter">
+      <Card>
         <Button onClick={() => alert('Button clicked!')}>It's ALIIIIIIIVEEEEE</Button>
-      </LegacyCard>
+      </Card>
     </Page>
   </AppProvider>
   );
