0a37f62e802184ad3a1c8e040dc0a29664481106
Kalen Jordan <kalen@kalens-laptop.lan>
2024-04-26 17:14:05 -0500

    Copy functionality and preview iframe

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 
 import enTranslations from '@shopify/polaris/locales/en.json';
-import { AppProvider, Page, Grid, Text, TextField, Card, InlineGrid, Button, FooterHelp, Link } 
-  from '@shopify/polaris';
+import { 
+  AppProvider, Page, Grid, Text, TextField, Card, 
+  InlineStack,Button, FooterHelp, Link,
+  Toast, Frame, Scrollable, BlockStack
+} from '@shopify/polaris';
 import '@shopify/polaris/build/esm/styles.css';
-import { useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { useLocalStorage } from "./useLocalStorage";
-import { Icon } from '@shopify/polaris';
 import { ClipboardIcon } from '@shopify/polaris-icons';
+import { useLiquid, RENDER_STATUS } from 'react-liquid'
 
 function App() {
 
@@ -18,6 +21,8 @@ function App() {
     'subtotal_price': 'order.subtotal_price | times: 100',
     'total_order_discount_amount': 'order.total_order_discount_amount | times: 100',
     'shipping_price': 'order.shipping_price',
+    'order_name': 'order.name',
+    /*
     'shipping_amount': '',
     'shipping_discount': '',
     'total_duties': '',
@@ -27,7 +32,6 @@ function App() {
     'due_at_date': '',
     'payment_terms.next_payment.amount_due': '',
     'consolidated_estimated_delivery_time': '',
-    'order_name': '',
     'po_number': '',
     'transactions': '',
     'transaction_count': '',
@@ -41,6 +45,7 @@ function App() {
     'billing_address': '',
     'company_location': '',
     'delivery_promise_branded_shipping_line': '',
+    */
   };
 
   const handleChange = useCallback(
@@ -58,48 +63,89 @@ function App() {
     [],
   );
 
+  const data = {
+      transactions: [],
+      order: {
+        subtotal_price: 10,
+        total_order_discount_amount: 0,
+      },
+      shop: {
+        name: 'Kalen Test Store'
+      }
+  }
+
+  const [active, setActive] = useState(false);
+
+  const toggleActive = useCallback(() => setActive((active) => !active), []);
+
+  const toastMarkup = active ? (
+    <Toast content="Copied Mechanic template to clipboard" onDismiss={toggleActive} />
+  ) : null;
+
+  const handleCopyButton = useCallback(() => {
+    navigator.clipboard.writeText(mechanicTemplate);
+    toggleActive();
+  }, []);
+
+  console.log("mechanic template", mechanicTemplate);
+
+  const { status, markup } = useLiquid(mechanicTemplate, data)
+  // let markup = 'test';
+
   return (
     <AppProvider i18n={enTranslations}>
+      <Frame>
       <Page title="Shopify To Mechanic Email Template Converter" 
       subtitle="Easily convert your Shopify email notification liquid templates to liquid that can be used in Mechanic tasks"
       >
-        <Grid>
-          <Grid.Cell columnSpan={{xs: 6, sm: 3, md: 3, lg: 6, xl: 6}}>
-            <Card sectioned>          
-              <Text as="h2" variant="headingSm">
-                Shopify Template
-              </Text>
-              <TextField
-                label="Copy in your Shopify email template liquid"
-                value={shopifyTemplate}
-                onChange={handleChange}
-                multiline={4}
-                autoComplete="off"
-              />
-            </Card>
-          </Grid.Cell>
-          <Grid.Cell columnSpan={{xs: 6, sm: 3, md: 3, lg: 6, xl: 6}}>
-            <Card sectioned>   
-              <div className="cardHeader">
-                <Text as="h2" variant="headingSm">
-                  Generated Mechanic template
-                </Text>
-                <Button icon={ClipboardIcon}>
-                  Copy
-                </Button>
-              </div>
-              
-              <TextField
-                label="Generated Mechanic email template liquid"
-                disabled
-                value={mechanicTemplate}
-                multiline={4}
-                autoComplete="off"
-              />             
-            </Card>
-          </Grid.Cell>
-          
-        </Grid>
+        <BlockStack gap="500">
+          <Grid>
+            <Grid.Cell columnSpan={{xs: 6, sm: 3, md: 3, lg: 6, xl: 6}}>
+              <Card sectioned>    
+                <BlockStack gap="300">
+                  <Text as="h2" variant="headingSm">
+                    Shopify Template
+                  </Text>
+                  <Scrollable style={{height: '300px'}}>
+                    <TextField
+                      value={shopifyTemplate}
+                      onChange={handleChange}
+                      multiline={4}
+                      autoComplete="off"
+                    />
+                  </Scrollable>
+                </BlockStack>      
+              </Card>
+            </Grid.Cell>
+            <Grid.Cell columnSpan={{xs: 6, sm: 3, md: 3, lg: 6, xl: 6}}>
+                <Card sectioned>
+                  <BlockStack gap="300">
+                  <InlineStack align="space-between">
+                    <Text as="h2" variant="headingSm">
+                      Generated Mechanic template
+                    </Text>
+                    <Button icon={ClipboardIcon} onClick={handleCopyButton}>
+                      Copy
+                    </Button>
+                    {toastMarkup}
+                  </InlineStack>
+                  <Scrollable style={{height: '300px'}}>
+                    <TextField
+                      disabled
+                      value={mechanicTemplate}
+                      multiline={4}
+                      autoComplete="off"
+                    />             
+                  </Scrollable>
+                  </BlockStack>
+                </Card>
+            </Grid.Cell>        
+          </Grid>
+          <Card>
+          <Text as="h2" variant="headingSm">Preview</Text>
+            <iframe srcDoc={markup} width="700" height="500"></iframe>
+          </Card>
+        </BlockStack>
         <FooterHelp>
           This is an {' '}
           <Link monochrome url="https://github.com/kalenjordan/mechanic-email-migrator">
@@ -113,6 +159,7 @@ function App() {
 
         </FooterHelp>
       </Page>
+      </Frame>
     </AppProvider>
   );
 }
